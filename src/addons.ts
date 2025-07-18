c09b784ab2ad8b1654d0132144fbd3d76e0f6035
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-18 16:28:22 +0700

    addons

---
+++ src/addons.ts
@@ -0,0 +1,40 @@
+/* check if a char, throw invalid argument */
+export function checkIfChar(c: string): void {
+    if (c.length !== 1)
+        throw new Error("invalid argument: non-char");
+}
+
+/* check if a char is an alphabet one */
+export function isAlpha(c: string): boolean {
+    checkIfChar(c);
+
+    return /^[a-z]$/i.test(c);
+}
+
+/* check if a char is a lowercase alphabet one */
+export function isLower(c: string): boolean {
+    checkIfChar(c);
+
+    return /^[a-z]$/.test(c);
+}
+
+/* check if a char is an uppercase alphabet one */
+export function isUpper(c: string): boolean {
+    checkIfChar(c);
+
+    return /^[A-Z]$/.test(c);
+}
+
+/* check if a char is a digit */
+export function isDigit(c: string): boolean {
+    checkIfChar(c);
+
+    return /^[0-9]$/.test(c);
+}
+
+/* check if a char is alphanumeric one */
+export function isAlNum(c: string): boolean {
+    checkIfChar(c);
+
+    return /^[a-z0-9]$/i.test(c);
+}
