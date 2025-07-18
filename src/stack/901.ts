9c8198a0013501954877ad9e3acd31f00b8795f9
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-18 17:08:37 +0700

    901

---
+++ src/stack/901.ts
@@ -0,0 +1,40 @@
+/* 901. online stock span */
+// #: stack
+
+class StockSpanner {
+    private readonly stack: [number, number][];
+
+    constructor() {
+        this.stack = new Array<[number, number]>();
+    }
+
+    public next(price: number): number {
+        let span = 1, len = this.stack.length;
+
+        while (len > 0 && price >= this.stack[len - 1][0]) {
+            span += this.stack[len - 1][1];
+            this.stack.pop();
+            --len;
+        }
+
+        this.stack.push([price, span]);
+
+        return span;
+    }
+
+    public static main(): void {
+        // add StockSpanner & prices
+        const stock: StockSpanner = new StockSpanner();
+        const prices: number[] = [100, 80, 60, 70, 60, 75, 85];
+
+        // calculate span for each day price
+        const ans: number[] = [];
+
+        for (let i = 0; i < prices.length; ++i)
+            ans.push(stock.next(prices[i]));
+
+        console.log(ans.join(" "));
+    }
+}
+
+StockSpanner.main();
