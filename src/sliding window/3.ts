9b5a07b6b4cd958fd37cf8000193fce456da6364
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-18 22:42:20 +0700

    3

---
+++ src/sliding window/3.ts
@@ -0,0 +1,31 @@
+/* 3. longest substring without repeating character */
+// #: sliding window + hash
+
+class src3 {
+    static lengthOfLongestSubstring(str: string): number {
+        const m: number = str.length;
+        const map = new Map<string, number>();
+        let l = 0, ans = 0;
+
+        for (let r = 0; r < m; ++r) {
+            if (map.has(str[r]))
+                l = Math.max(l, map.get(str[r])! + 1);
+
+            map.set(str[r], r);
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+
+    public static main(): void {
+        // add string
+        const str: string = "zxyzyxz";
+
+        // maxima length substring of non-repeating chars
+        let ans: number = src3.lengthOfLongestSubstring(str);
+        console.log(ans);
+    }
+}
+
+src3.main();
