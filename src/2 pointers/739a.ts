0894b5a1097a97d296dd598e50690be316c591ba
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-18 16:50:20 +0700

    739

---
+++ src/2 pointers/739a.ts
@@ -0,0 +1,38 @@
+/* 739a. daily temperatures */
+// #: 2 pointers + dp
+
+class src739a {
+    static dailyTemperatures(tmps: number[]): number[] {
+        const m: number = tmps.length;
+        const ans = new Array<number>(m).fill(0);
+
+        for (let i = m - 2; i >= 0; --i) {
+            let j = i + 1;
+
+            while (j < m && tmps[j] <= tmps[i]) {
+                if (ans[j] === 0) {
+                    j = m;
+                    break;
+                }
+
+                j += ans[j];
+            }
+
+            if (j !== m)
+                ans[i] = j - i;
+        }
+
+        return ans;
+    }
+
+    public static main(): void {
+        // add temperatures
+        const tmps: number[] = [30, 38, 30, 36, 35, 40, 28];
+
+        // calculate wait time until warmer day
+        let ans: number[] = src739a.dailyTemperatures(tmps);
+        console.log(ans.join(" "));
+    }
+}
+
+src739a.main();
---
+++ src/stack/739b.ts
@@ -0,0 +1,37 @@
+/* 739b. daily temperatures */
+// #: 2 pointers + dp
+
+class src739b {
+    static dailyTemperatures(tmps: number[]): number[] {
+        const m: number = tmps.length;
+
+        const ans = new Array<number>(m).fill(0);
+        const stack = new Array<[number, number]>();
+
+        for (let i = 0; i < m; ++i) {
+            let len = stack.length;
+
+            while (len > 0 && tmps[i] > stack[len - 1][0]) {
+                ans[stack[len - 1][1]] = i - stack[len - 1][1];
+
+                stack.pop();
+                --len;
+            }
+
+            stack.push([tmps[i], i]);
+        }
+
+        return ans;
+    }
+
+    public static main(): void {
+        // add temperatures
+        const tmps: number[] = [30, 38, 30, 36, 35, 40, 28];
+
+        // calculate wait time until warmer day
+        let ans: number[] = src739b.dailyTemperatures(tmps);
+        console.log(ans.join(" "));
+    }
+}
+
+src739b.main();
