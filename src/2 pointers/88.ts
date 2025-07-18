c00880246ba60fc65bfe73a2061a751c6f1d0c04
namnkahn1607 <nlnkandthebois@gmail.com>
2025-07-18 19:40:06 +0700

    88

---
+++ src/2 pointers/88.ts
@@ -0,0 +1,40 @@
+/* 88. merge sorted arrays */
+// #: 2 pointers
+
+class src88 {
+    static merge(nums1: number[], m: number, nums2: number[], n: number): void {
+        let i = m - 1, j = n - 1, k = m + n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] >= nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+
+        while (i >= 0)
+            nums1[k--] = nums1[i--];
+
+        while (j >= 0)
+            nums1[k--] = nums2[j--];
+    }
+
+    public static main(): void {
+        // add arrays
+        const nums1: number[] = [10, 20, 20, 40];
+        const m: number = nums1.length;
+
+        const nums2: number[] = [1, 2];
+        const n: number = nums2.length;
+
+        for (let i = 0; i < n; ++i)
+            nums1.push(0);
+
+        // merge 2 sorted arrays into one
+        src88.merge(nums1, m, nums2, n);
+
+        console.log(nums1.join(" "));
+    }
+}
+
+src88.main();
